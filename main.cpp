a306b8845d673d2b925a48935080a0582d831bcc
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2026-02-01 14:16:55 -0500

    Init

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <random>
+
+class Reel
+{
+public:
+    virtual char spin()
+    {
+
+    }
+
+};
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
