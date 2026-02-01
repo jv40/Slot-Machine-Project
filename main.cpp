56a15adc9291a928e75603592f2e0c3a13b4e68c
Jonathan Vasquez <vasquez_jonathan@wheatoncollege.edu>
2026-02-01 16:51:56 -0500

    Fixed reelChars array logic to output Unicode character to proper odds

--- main.cpp
+++ main.cpp
@@ -5,27 +5,53 @@ using namespace std;
 class Reel
 {
 public:
-    virtual char spin()
+    virtual string spin()
     {
-        char reelCharChosen = reelCharArray[randomNumberGenerator(REEL_MIN,REEL_MAX)];
+        string reelCharChosen = reelChars[randomNumberGenerator(REEL_MIN,REEL_MAX)];
         cout << reelCharChosen;
         return (reelCharChosen);
     }
 
 private:
-    char reelCharArray[10] = {'0','1','2','3','4','5','6','7','=', '@'};
+    string reelChars[10] = {
+        "\u274C", "\u274C", "\u274C", "\u274C",  // 50% chance
+        "\u2757", "\u2757", "\u2757",               // 30% chance
+        "\u2705", "\u2705",                            // 20% chance
+        "7"                                               // 10% chance
+    };
     double REEL_MIN = 0;
     double REEL_MAX = 9;
 
+    std::random_device r; // We seed this?
+    std::default_random_engine e1{r()}; // Making random engine seeded with random device r()
+
     long randomNumberGenerator(double minimum, double maximum)
     {
-        std::random_device r; // We seed this?
-        std::default_random_engine e1(r()); // Making random engine seeded with random device r()
 
         std::uniform_int_distribution <long> prob(minimum,maximum);
         long randomNumber = prob(e1);
         return(randomNumber);
     }
+};
+
+//Can use reel public functions but is not a reel
+//Use #include reel when putting it in its own .h file
+class SlotMachine
+{
+public:
+    bool virtual checkWin();
+};
+
+class ThreeReelSlotMachine: public SlotMachine
+{
+public:
+    bool checkWin() override
+    {
+        //
+    }
+
+private:
+    //
 };
 
 int main()
